63d392eb4b66c81f25467e7c1294305191910e43
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-12-06 15:04:09 +0530

    practice

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import LogIn from './pages/LogIn'
+import Order from './pages/Order'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
+const App = () => {
+  return (
+    <div className='px-2 sm:px-[4vw] md:px-[6vx], lg:px-[8vw]'>
+      <Navbar />
+      <Routes>
+<Route path='/'element={<Home />} />
+<Route path='/collection'element={<Collection />} />
+<Route path='/about'element={<About />} />
+<Route path='/contact'element={<Contact />} />
+<Route path='/logIn'element={<LogIn />} />
+<Route path='/orders'element={<Order />} />
+<Route path='/place-order'element={<PlaceOrder />} />
+<Route path='/product/product:Id'element={<Product />} />
+<Route path='/cart'element={<Cart />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,174 @@
+import p_img1 from '../assets/Product/7-4-v-1100-mah-rechargeable-lithium-ion-battery-500x500.jpg'
+import p_img2 from '../assets/Product/7-4-v-2200-mah-lithium-battery-500x500.jpg'
+import p_img3 from '../assets/Product/7-4-v-2600-mah-lithium-ion-battery-500x500.jpg'
+import p_img4 from '../assets/Product/11-1-v-2200-mah-lithium-ion-battery-500x500.jpg'
+import p_img5 from '../assets/Product/1100-mah-lithium-ion-battery-500x500 (1).jpg'
+import p_img6 from '../assets/Product/2200-mah-lithium-ion-battery-500x500.jpg'
+import p_img7 from '../assets/Product/2200-mah-rechargeable-lithium-ion-battery-500x500 (1).jpg'
+import p_img8 from '../assets/Product/2200-mah-rechargeable-lithium-ion-battery-500x500 (1).jpg'
+import p_img9 from '../assets/Product/2600-mah-lithium-ion-battery-500x500 (1).jpg'
+import p_img10 from '../assets/Product/4400-mah-lithium-ion-battery-500x500.jpg'
+// import B_img1 from '../assets/Banner/AdobeStock_459574340_Preview.jpeg'
+// import B_img2 from '../assets/Banner/AdobeStock_526620639_Preview.jpeg'
+// import B_img3 from '../assets/Banner/AdobeStock_905269181_Preview.jpeg'
+
+
+
+import logo from './logo-color.png'
+import hero_img from '../assets/Banner/AdobeStock_459574340_Preview.jpeg'
+import cart_icon from './cart_icon.png'
+import bin_icon from './bin_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_img from './support_img.png'
+import menu_icon from './menu_icon.png'
+
+
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+
+export const assets = {
+    logo,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_img,
+    menu_icon,
+    
+  
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+}
+
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "7-4-v-1100-mah-rechargeable-lithium-ion-battery",
+        description: "7-4-v-1100-mah-rechargeable-lithium-ion-battery",
+        price: 200,
+        image: [p_img1],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+        
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaab",
+        name: "7-4-v-2200-mah-lithium-battery",
+        description: "7-4-v-2200-mah-lithium-battery",
+        price: 200,
+        image: [p_img2],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+    
+        date: 1716621345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaac",
+        name: "7-4-v-2600-mah-lithium-ion-battery",
+        description: "7-4-v-2600-mah-lithium-ion-battery",
+        price: 220,
+        image: [p_img3],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+      
+        date: 1716234545448,
+        bestseller: true
+    },
+    {
+        _id: "aaaad",
+        name: "11-1-v-2200-mah-lithium-ion-battery",
+        description: "11-1-v-2200-mah-lithium-ion-battery",
+        price: 110,
+        image: [p_img4],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+        
+        date: 1716621345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaae",
+        name: "1100-mah-lithium-ion-battery",
+        description: "1100-mah-lithium-ion-battery",
+        price: 130,
+        image: [p_img5],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+     
+        date: 1716622345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaf",
+        name: "",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 140,
+        image: [p_img6],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "L", "XL"],
+        date: 1716623423448,
+        bestseller: true
+    },
+    {
+        _id: "aaaag",
+        name: "2200-mah-rechargeable-lithium-ion-battery",
+        description: "2200mAh Rechargeable Lithium-Ion Battery",
+        price: 190,
+        image: [p_img7],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+        date: 1716621542448,
+        bestseller: false
+    },
+    {
+        _id: "aaaah",
+        name: "2200-mah-rechargeable-lithium-ion-battery",
+        description: "2200mAh Rechargeable Lithium-Ion Battery",
+        price: 140,
+        image: [p_img8],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+        date: 1716622345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaai",
+        name: "2600-mah-lithium-ion-battery",
+        description: "2600mAh Lithium-Ion Battery",
+        price: 100,
+        image: [p_img9],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+        date: 1716621235448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaj",
+        name: "4400-mah-lithium-ion-battery",
+        description: "4400mAh Lithium-Ion Battery",
+        price: 110,
+        image: [p_img10],
+        category: "Lithium",
+        subCategory: "Lithium-ion",
+        date: 1716622235448,
+        bestseller: false
+    },
+]

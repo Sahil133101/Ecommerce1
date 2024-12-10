e3a4fba61173449fc8919494f936b10654ed1718
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-12-10 22:58:07 +0530

    commit

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { Shopcontext } from '../context/Shopcontext'
 
 const Cart = () => {
+
+  const {products, currency, cartItems} = useContext(Shopcontext);
+
+  const [cartData, setcartData] = useState([]);
+
+  useEffect(()=>{
+const tempData = [];
+for(const items in cartItems){
+  for(const item in cartItems[items]){
+    if(cartItems[items][item]>0){
+      tempData.push({
+        _id: items,
+        size : items,
+        quantity: cartItems[items][item]
+      })
+
+    }
+  }
+  console.log(tempData)
+}
+  },[cartItems])
   return (
     <div>Cart</div>
   )

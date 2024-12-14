48f3ed38962f09012efd5c31edde47a2fbbd6909
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-12-14 11:56:44 +0530

    commit

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { Shopcontext } from '../context/Shopcontext'
+import Title from '../components/Title';
 const Order = () => {
+  const{products,currency} = useContext(Shopcontext);
   return (
-    <div>Order</div>
+    <div className='border-t pt-16'>
+      <div className='text-2xl'>
+<Title text1={'MY'} text2={'ORDERS'} />
+      </div>
+      <div className=''>
+        {
+          products.slice(1,4).map((item,index)=>(
+            <div ley={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:justify-between gap-4'>
+              <div className='flex flex-start gap-6 text-sm'>
+                <img  className='w-16 sm:w-20'src={item.image[0]} />
+                <div>
+                  <p>{item.name}</p>
+                  <p>{currency}{item.price}</p>
+                </div>
+              </div>
+              </div>
+          ) 
+          )
+        }
+
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
+import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
+import { useContext } from 'react'
+import { Shopcontext } from '../context/Shopcontext'
 
 const PlaceOrder = () => {
+  const[method, setmethod] = useState('cod') 
+  const {navigate} = useContext(Shopcontext)
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t' >
       {/* left side */}
@@ -36,6 +42,41 @@ const PlaceOrder = () => {
         </div>
        
       </div>  
+      {/* right side */}
+      <div className='mt-8'>
+        <div className='mt-8 min-w-80'>
+          <CartTotal />
+
+        </div>
+        <div className='mt-12'>
+          <Title text1={'PAYEMENT'}text2={'METHOD'} />
+          {/* payment method selection */}
+          <div className='flex gap-3 flex-col lg:flex-row '>
+
+            <div onClick={()=>setmethod('stripe')}className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full  ${method === 'stripe' ? 'bg-green-400':''}`}></p>
+              <img className='h-5 mx-4' src={assets.stripe_logo}/>
+
+            </div>
+                        <div onClick={()=>setmethod('razarpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full  ${method === 'razarpay' ? 'bg-green-400':''}`}></p>
+              <img className='h-5 mx-4' src={assets.razorpay_logo}/>
+
+            </div>
+            <div onClick={()=>setmethod('cod')}className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400':''}`}></p>
+             <p className='text-gray-500 text-sm font-medium mx-4'> CASH ON DELIVERY</p>
+
+            </div>
+
+          </div>
+          <div className='w-full text-end mt-8'>
+            <button onClick={()=> navigate('/orders')} className='bg-black text-white px-16 py-3'>PLACE ORDER</button>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
